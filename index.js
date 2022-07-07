c835d3e1a5395d89e92d839a5a960c4c35002d48
shil <shilpashilus39@gmail.com>
2022-07-07 17:03:06 +0530

    RemainderServer

---
+++ index.js
@@ -0,0 +1,10 @@
+//import express
+const express = require('express')
+
+//use express
+const app = express()
+
+//set port number
+app.listen(3000,()=>{
+    console.log("Server started at port 3000");
+})
